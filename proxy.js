a4f20d656a750470e8f71276499577cf48594ffb
SammieLJ <samir.subasic@gmail.com>
2023-11-22 23:13:04 +0100

    Update version of proxy and server js files
    I have update these two files, when I did some testing on my MacBook. There were some running errors which did't show on my Windows 10 machine.

--- proxy.js
+++ proxy.js
@@ -1,50 +1,51 @@
 // proxy.js
 const express = require('express');
-const fetch = require('node-fetch');
 const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
-// Export the aggregateData function for use in server.js
-module.exports = {
-  aggregateData: async (selectedDateTimeFrom, selectedDateTimeTo) => {
-    try {
-      let data;
+const aggregateData = async (selectedDateTimeFrom, selectedDateTimeTo) => {
+  try {
+    // Use dynamic import for node-fetch
+    const fetch = (await import('node-fetch')).default;
 
-      // Check if allData.json exists
-      if (fs.existsSync('allData.json')) {
-        // If the file exists, read its content
-        data = JSON.parse(fs.readFileSync('allData.json'));
-      } else {
-        // If the file doesn't exist, make the API call
-        const apiURL = 'https://trzic.musiclab.si/api/gorenjskabike';
-        const response = await fetch(apiURL);
-        data = await response.json();
+    let data;
 
-        // Save the API response to the file
-        fs.writeFileSync('allData.json', JSON.stringify(data));
-      }
+    // Check if allData.json exists
+    if (fs.existsSync('allData.json')) {
+      // If the file exists, read its content
+      data = JSON.parse(fs.readFileSync('allData.json'));
+    } else {
+      // If the file doesn't exist, make the API call
+      const apiURL = 'https://trzic.musiclab.si/api/gorenjskabike';
+      const response = await fetch(apiURL);
+      data = await response.json();
 
-      // Filter data based on the provided date range
-      if (selectedDateTimeFrom && selectedDateTimeTo) {
-        const startDate = new Date(selectedDateTimeFrom);
-        const endDate = new Date(selectedDateTimeTo);
-
-        // Filter data for the selected date range
-        data = data.filter(entry => {
-          const entryDate = new Date(entry.timestamp);
-          return entryDate >= startDate && entryDate <= endDate;
-        });
-      }
-
-      return data;
-    } catch (error) {
-      console.error('Error fetching and aggregating data:', error);
-      throw error;
+      // Save the API response to the file
+      fs.writeFileSync('allData.json', JSON.stringify(data));
     }
-  },
+
+    // Filter data based on the provided date range
+    if (selectedDateTimeFrom && selectedDateTimeTo) {
+      const startDate = new Date(selectedDateTimeFrom);
+      const endDate = new Date(selectedDateTimeTo);
+
+      // Filter data for the selected date range
+      data = data.filter(entry => {
+        const entryDate = new Date(entry.timestamp);
+        return entryDate >= startDate && entryDate <= endDate;
+      });
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching and aggregating data:', error);
+    throw error;
+  }
 };
+
+module.exports = aggregateData;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-// server.js
 const express = require('express');
+const aggregateData = require('./proxy');
 const app = express();
 const port = 3001;
 
-const { aggregateData } = require('./proxy');
-
-app.use(express.json()); // Parse JSON requests
+app.use(express.json());
 
 app.use((req, res, next) => {
-  //res.setHeader('Content-Security-Policy', 'default-src *');
-  res.header('Access-Control-Allow-Origin', '*'); // You can restrict this to specific origins for security
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
